3d07f1b4ac1632213d7335a3250cb0c036e9b5e9
Clement Davin <davi5059@csumb.ad.csumb.edu>
2018-02-13 11:47:34 -0800

    first

---
+++ SuperHot/Assets/scripts/LevelManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+    public GameObject[] rooms;
+    public GameObject prefab;
+
+    // Use this for initialization
+    void Start () {
+        for (int i = 0; i < 3; i++)
+        {
+            Instantiate(prefab, rooms[i].transform.position + new Vector3(Random.insideUnitCircle.x, 0, Random.insideUnitCircle.y), Quaternion.identity);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+}
