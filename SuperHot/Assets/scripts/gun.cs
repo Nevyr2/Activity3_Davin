e341bcf1909175dceb4ad77b125819db66fa40a4
DESKTOP-T1LO9OM\cleme <cdavin@csumb.edu>
2018-02-23 21:38:46 -0800

    last verison

--- SuperHot/Assets/scripts/gun.cs
+++ SuperHot/Assets/scripts/gun.cs
@@ -4,57 +4,85 @@ using UnityEngine;
 
 public class gun : MonoBehaviour {
 
-    public float damage = 10f;
-    public float range = 100f;
-    public ParticleSystem fire_particle;
-    public float ImpactForce = 100f;
-    Vector3 random_ball;
+
+    int gunDamage = 5;
+    public float fireRate = 0.25f;
+    public float weaponRange = 100f;
+    public float hitForce = 100f;
+    public Transform gunEnd;
+
     public GameObject first_collider;
     public bool dead = false;
 
     public GameObject ImpactEffect;
+    private AudioSource gunAudio;
 
     public Camera fpsCam;
+    private WaitForSeconds shotDuration = new WaitForSeconds(0.07f);
+    private LineRenderer laserLine;
+    private float nextFire;
 	
 
-	void Update ()
+
+    void Start()
     {
-		if (Input.GetKeyDown(KeyCode.Space))
+        laserLine = GetComponent<LineRenderer>();
+    }
+
+    void Update ()
+    {
+    	if (Input.GetKeyDown(KeyCode.Space) && Time.time > nextFire)
         {
             if (!dead)
             {
                 first_collider.GetComponent<BoxCollider>().enabled = false;
-                Shoot();
+
+	        	nextFire = Time.time + fireRate;
+
+                StartCoroutine(ShotEffect());
+
+                Vector3 rayOrigin = fpsCam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0));
+
+                RaycastHit hit;
+
+                laserLine.SetPosition(0, gunEnd.position);
+
+                if (Physics.Raycast(rayOrigin, fpsCam.transform.forward, out hit, weaponRange))
+                {
+                    laserLine.SetPosition(1, hit.point);
+
+                    target Target = hit.transform.GetComponent<target>();
+                    BoxCollider Target_collider = hit.transform.GetComponent<BoxCollider>();
+                    if (Target != null && !Target_collider.isTrigger)
+                    {
+                        Target.TakeDamage(gunDamage);
+                        GameObject impact = Instantiate(ImpactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+                        Destroy(impact, 0.5f);
+                    }
+
+                    if (hit.rigidbody != null)
+                    {
+                        hit.rigidbody.AddForce(-hit.normal * hitForce);
+                    }
+                }
+                else
+                {
+                    laserLine.SetPosition(1, rayOrigin + (fpsCam.transform.forward * weaponRange));
+                }
+
                 first_collider.GetComponent<BoxCollider>().enabled = true;
             }
-            fire_particle.Play();
         }
 	}
 
-    void Shoot()
+    
+
+    private IEnumerator ShotEffect()
     {
-        RaycastHit hit;
-
-        random_ball.x = fpsCam.transform.position.x - Random.Range(-0.05f, 0.05f);
-        random_ball.y = fpsCam.transform.position.y - Random.Range(-0.05f, 0.05f);
-        random_ball.z = fpsCam.transform.position.z - Random.Range(-0.05f, 0.05f);
-
-        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
-        {
-            target Target = hit.transform.GetComponent<target>();
-            BoxCollider Target_collider = hit.transform.GetComponent<BoxCollider>();
-            if (Target != null && !Target_collider.isTrigger)
-            {
-                Target.TakeDamage(damage);
-                GameObject impact = Instantiate(ImpactEffect, hit.point, Quaternion.LookRotation(hit.normal));
-                Destroy(impact, 0.5f);
-            }
-
-            if (hit.rigidbody != null)
-            {
-                hit.rigidbody.AddForce(-hit.normal * ImpactForce);
-            }
-
-        }
+        gunAudio.Play();
+    	laserLine.enabled = true;
+        yield return shotDuration;
+	    laserLine.enabled = false;
     }
+
 }
